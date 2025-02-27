5fc0099443927f74e6b74657885f7620b528047f
shawbyoung <shawbyoung@gmail.com>
2025-02-26 20:11:32 -0500

    altered main to reflect changes to engine.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from engine import engine
 
+# Drivers
+from betmgm import betmgm
+from betrivers import betrivers
+from draftkings import draftkings
+from hardrock import hardrock
+from fanduel import fanduel
+
 def main():
-    e = engine()
+    drivers = {
+        'fanduel' : fanduel(),
+        'betmgm' : betmgm(),
+        'draftkings' : draftkings(),
+        'betrivers' : betrivers()
+    }
+    e = engine(drivers)
     e.run()
 
 if __name__ == '__main__':
