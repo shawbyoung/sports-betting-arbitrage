ecfe7b10beb555256a7c91ee837db05b28ace5ca
shawbyoung <shawbyoung@gmail.com>
2025-02-26 20:06:22 -0500

    updated webdriver initialization, impl general login form entry, impl general execute bet method (wip/mvp), impl retry logic in login

--- driver.py
+++ driver.py
@@ -17,22 +17,29 @@ from odds import odds
 class driver:
 	def __init__(self, name):
 		self.name = name
-		self.username = None
-		self.password = None
-		self.initialize_webdriver()
+		self._username = None
+		self._password = None
+		self._user_data_dir = None
+
+	def set_user_data_dir(self, user_data_dir):
+		self._user_data_dir = user_data_dir
 
 	def initialize_webdriver(self) -> None:
-		self._log(f'Initializing web driver.')
-		service = Service(executable_path='chromedriver/chromedriver.exe')
+		self._log(f'Initializing {self.name} web driver.')
+		self._log(f'Using \'{self._user_data_dir}\' as user_data_dir.')
+		service = Service(executable_path=util.chromedriver_path, service_args=['--log-level=DEBUG'])
 		options = Options()
 		user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0"
 		options.add_argument(f"user-agent={user_agent}")
 		options.binary_location = 'chrome/chrome-win64/chrome.exe'
+		options.add_argument(f'--user-data-dir={self._user_data_dir}')
 		options.add_argument('--disable-blink-features')
 		options.add_argument('--disable-blink-features=AutomationControlled')
 		options.add_experimental_option("excludeSwitches", ["enable-automation"])
 		options.add_experimental_option('useAutomationExtension', False)
+		options.add_argument("--no-sandbox")
 		self.driver = webdriver.Chrome(service=service, options=options)
+		self._log(f'Initialized web driver.')
 		self.driver.execute_script(
 			"Object.defineProperty(navigator, 'webdriver', {"
 				"get: () => undefined"
@@ -41,7 +48,8 @@ class driver:
 		navigator_webdriver = self.driver.execute_script("return navigator.webdriver;")
 		if navigator_webdriver:
 			self._log(f'Webdriver NOT set to undefined, set to {navigator_webdriver}. Detection possible.')
-		self._log(f'Initialized web driver.')
+			exit(1)
+		self._log(f'Executed scripts.')
 
 	def set_password(self, password) -> None:
 		self.password = password
@@ -65,18 +73,26 @@ class driver:
 
 		log_fn(f'[{self.name}] {message}', level)
 
+	def _login_form_entry(self, username_input, password_input, submit_button):
+		util.simulate.short_interaction_time()
+		util.simulate.type_in_field(username_input, self.username)
+		util.simulate.short_interaction_time()
+		util.simulate.type_in_field(password_input, self.password)
+		util.simulate.click_short_wait(submit_button)
+
 	def _login_aux(self) -> None:
 		self.driver.get('www.example.com')
 
 	def login(self) -> bool:
-		self._log(f'Logging in.')
-		try:
-			self._login_aux()
-		except Exception as e:
-			self._log(f'Failed to log in. {e}')
-			return True
-
-		self._log(f'Logged in.')
+		for idx in range(util.max_login_retries):
+			self._log(f'Log in attempt {idx}.')
+			try:
+				self._login_aux()
+				self._log(f'Logged in.')
+				return True
+			except Exception as e:
+				self._log(f'Failed to log in. {e}')
+				util.simulate.exact_wait(10)
 		return False
 
 	def _get_promotion_link(self):
@@ -84,13 +100,16 @@ class driver:
 
 	# Gets the appropriate promotion page for `collect_promotion_odds`. 
 	# Logs an error if the page is unreachable.
-	def _get_promotion_page(self):
+	def _get_promotion_page(self) -> bool:
 		try:
 			link = self._get_promotion_link()
-			if link != driver.current_url:
+			cur_url = self.driver.current_url
+			if link != cur_url:
 				self.driver.get(self._get_promotion_link())
-		except:
-			self._log(f'Cannot get {util.promotion} page.', 'error')
+			return True
+		except Exception as e:
+			self._log(f'Cannot get {util.promotion} page. {e}', 'error')
+			return False
 
 	def _get_events_aux(self):
 		pass
@@ -112,7 +131,9 @@ class driver:
 	# Generic function for collecting odds from a promotion after getting the 
  	# appropriate promotion link.
 	def get_odds(self):
-		response = self._get_promotion_page()
+		if not self._get_promotion_page():
+			return []
+
 		events = self._get_events()
 		odds = []
 		for event in events:
@@ -120,3 +141,28 @@ class driver:
 			if event_odds:
 				odds.append(event_odds)
 		return odds
+
+	def _get_event_element(self, team):
+		events = self._get_events()
+		for event in events:
+			if event.text.contains(team):
+				return event
+		return None
+
+	def _get_moneyline_bet_button(self, event, team):
+		return None
+
+	def execute_bet(self, team, wager) -> bool:
+		if not self._get_promotion_page():
+			return False
+
+		event = self._get_event_element(team)
+		if not event:
+			return False
+
+		button = self._get_moneyline_bet_button(event, team)
+		if not button:
+			return False
+
+		button.click()
+		return True
