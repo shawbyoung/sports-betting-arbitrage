98808aef0a48944c4d54de6dab9183a6fdd2c6f3
shawbyoung <shawbyoung@gmail.com>
2025-02-27 16:42:24 -0500

    made changes to engine reflect new classes odds, event, and bet_request, changes to util and driver

--- engine.py
+++ engine.py
@@ -8,6 +8,9 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from driver import driver
 from logger import logger
+from odds import odds
+from event import event
+from bet_request import bet_request
 
 # Drivers
 from betmgm import betmgm
@@ -17,6 +20,8 @@ from hardrock import hardrock
 from fanduel import fanduel
 
 class engine:
+	events_map_ty = dict[tuple[str,str], event]
+
 	def __init__(self, drivers):
 		self.drivers = drivers
 		self.config()
@@ -77,7 +82,7 @@ class engine:
 				exit(1)
 
 			self.drivers[sportsbook].set_username(username)
-			self.drivers[sportsbook].password = password
+			self.drivers[sportsbook].set_password(password)
 
 		self.set_profiles(config)
 
@@ -123,7 +128,7 @@ class engine:
 					logger.log_error(f"Error with driver {driver_obj}: {e}")
 					# TODO: Change `results[driver_obj]` back to None and add smth to handle that in
 					# bet so that _run_on_all_drivers returns output that's handle-able by a multitude of tasks.
-					results[driver_obj] = []
+					results[driver_obj] = None
 
 		return results
 
@@ -135,108 +140,145 @@ class engine:
 			return d.login()
 		logger.log('Logging into sportsbooks.')
 		login_success = self._run_on_all_drivers(task)
-		dead_sportsbooks = [sportbook.name for sportbook, success in login_success.items() if success == False]
+		dead_sportsbooks = [sportbook.get_name() for sportbook, success in login_success.items() if success == False]
 		for dead_sportsbook in dead_sportsbooks:
 			self.drop_sportsbook(dead_sportsbook)
 
-	def	_find_polarizing_odds(self, odds):
-		events = {}
-		for odds in odds:
-			sportsbook = odds['sportsbook']
-			t1_name, t2_name = odds['t1_name'], odds['t2_name']
-			t1_moneyline_odds, t2_moneyline_odds = odds['t1_moneyline_odds'], odds['t2_moneyline_odds']
-
-			if (t1_name, t2_name) not in events:
-				events[t1_name, t2_name] = {
-					't1_name' : t1_name,
-					't2_name' : t2_name,
-					't1_moneyline_odds_min' : t1_moneyline_odds,
-					't1_moneyline_odds_min_sportsbook' : sportsbook,
-					't1_moneyline_odds_max' : t1_moneyline_odds,
-					't1_moneyline_odds_max_sportsbook' : sportsbook,
-					't2_moneyline_odds_min' : t2_moneyline_odds,
-					't2_moneyline_odds_min_sportsbook' : sportsbook,
-					't2_moneyline_odds_max' : t2_moneyline_odds,
-					't2_moneyline_odds_max_sportsbook' : sportsbook
-				}
+	def	_find_polarizing_odds(self, all_odds: list[odds]):
+		events: engine.events_map_ty = {}
+		for sb_odds in all_odds:
+			e: event | None = events.get((sb_odds.get_t1_name(), sb_odds.get_t2_name()), None)
+			if not e:
+				events[sb_odds.get_t1_name(), sb_odds.get_t2_name()] = event(
+					sb_odds.get_t1_name(),
+					sb_odds.get_t2_name(),
+					sb_odds.get_t1_odds(),
+					sb_odds.get_sportsbook(),
+					sb_odds.get_t1_odds(),
+					sb_odds.get_sportsbook(),
+					sb_odds.get_t2_odds(),
+					sb_odds.get_sportsbook(),
+					sb_odds.get_t2_odds(),
+					sb_odds.get_sportsbook(),
+				)
 				continue
 
-			event = events[t1_name, t2_name]
-			if t1_moneyline_odds < event['t1_moneyline_odds_min']:
-				event['t1_moneyline_odds_min'] = t1_moneyline_odds
-				event['t1_moneyline_odds_min_sportsbook'] = sportsbook
+			if sb_odds.get_t1_odds() < e.get_t1_min():
+				e.update_t1_min(sb_odds.get_t1_odds(), sb_odds.get_sportsbook())
 
-			if t1_moneyline_odds > event['t1_moneyline_odds_max']:
-				event['t1_moneyline_odds_max'] = t1_moneyline_odds
-				event['t1_moneyline_odds_max_sportsbook'] = sportsbook
+			if sb_odds.get_t1_odds() > e.get_t1_max():
+				e.update_t1_max(sb_odds.get_t1_odds(), sb_odds.get_sportsbook())
 
-			if t2_moneyline_odds < event['t2_moneyline_odds_min']:
-				event['t2_moneyline_odds_min'] = t2_moneyline_odds
-				event['t2_moneyline_odds_min_sportsbook'] = sportsbook
+			if sb_odds.get_t2_odds() < e.get_t2_min():
+				e.update_t2_min(sb_odds.get_t2_odds(), sb_odds.get_sportsbook())
 
-			if t2_moneyline_odds > event['t2_moneyline_odds_max']:
-				event['t2_moneyline_odds_max'] = t2_moneyline_odds
-				event['t2_moneyline_odds_max_sportsbook'] = sportsbook
+			if sb_odds.get_t2_odds() > e.get_t2_max():
+				e.update_t2_max(sb_odds.get_t2_odds(), sb_odds.get_sportsbook())
 
 		return events
 
-	def _find_arbitrage(self, odds):
-		events = self._find_polarizing_odds(odds)
+	def _find_arbitrage(self, all_odds: list[odds]) -> list[bet_request]:
+		events: engine.events_map_ty = self._find_polarizing_odds(all_odds)
+		bet_amt: float = 100.0
 
-		for (t1_name, t2_name), event in events.items():
-			favorite, underdog, favorite_odds, underdog_odds = None, None, None, None
-			bet_amt = 100
-			if util.compute_arb(event['t1_moneyline_odds_min'], event['t2_moneyline_odds_max']) < 1:
-				favorite = event['t1_name']
-				underdog = event['t2_name']
-				favorite_sportsbook = event['t1_moneyline_odds_min_sportsbook']
-				underdog_sportsbook = event['t2_moneyline_odds_max_sportsbook']
-				favorite_odds = event['t1_moneyline_odds_min']
-				underdog_odds = event['t2_moneyline_odds_max']
-			elif util.compute_arb(event['t1_moneyline_odds_max'], event['t2_moneyline_odds_min']) < 1:
-				favorite = event['t2_name']
-				underdog = event['t1_name']
-				favorite_sportsbook = event['t1_moneyline_odds_max_sportsbook']
-				underdog_sportsbook = event['t2_moneyline_odds_min_sportsbook']
-				favorite_odds = event['t2_moneyline_odds_min']
-				underdog_odds = event['t1_moneyline_odds_max']
-			else:
-				t1_moneyline_odds_min = event['t1_moneyline_odds_min']
-				t1_moneyline_odds_min_sportsbook = event['t1_moneyline_odds_min_sportsbook']
-				t1_moneyline_odds_max = event['t1_moneyline_odds_max']
-				t1_moneyline_odds_max_sportsbook = event['t1_moneyline_odds_max_sportsbook']
-				t2_moneyline_odds_min = event['t2_moneyline_odds_min']
-				t2_moneyline_odds_min_sportsbook = event['t2_moneyline_odds_min_sportsbook']
-				t2_moneyline_odds_max = event['t2_moneyline_odds_max']
-				t2_moneyline_odds_max_sportsbook = event['t2_moneyline_odds_max_sportsbook']
+		def log_arb_found(bet_request: list[bet_request]):
+			assert len(bet_request) == 2, 'len(bet requests) neq 2.'
+			f, u = bet_request
+			logger.log(
+				f'Arbitrage found! Bet {f.get_wager()} on {f.get_team()} through {f.get_sportsbook()} and '
+				f'{u.get_wager()} on {u.get_team()} through {u.get_sportsbook()}.'
+			)
+			logger.log(
+				f'Favorite wager = {bet_amt} / (({f.get_odds():.2f}/{u.get_odds():.2f}) + 1 ) ~= {f.get_wager()}.'
+			)
+			logger.log(
+				f'Underdog wager = ({bet_amt}*{f.get_odds():.2f}) / ({f.get_odds():.2f} + {u.get_odds():.2f}) ~= {u.get_wager()}.'
+			)
+			logger.log(
+				f'Profit = {util.compute_winnings(f.get_wager(), f.get_odds(), u.get_wager(), u.get_odds())}'
+			)
 
-				logger.log(
-        			f'Arbitrage not found. Best odds on {t1_name} are {t1_moneyline_odds_min:.2f} ({t1_moneyline_odds_min_sportsbook}) '
-               		f'and {t1_moneyline_odds_max:.2f} ({t1_moneyline_odds_max_sportsbook}).'
-               	)
-				logger.log(
-        			f'Best odds on {t2_name} are {t2_moneyline_odds_min:.2f} ({t2_moneyline_odds_min_sportsbook}) '
-					f'and {t2_moneyline_odds_max:.2f} ({t2_moneyline_odds_max_sportsbook}).'
-           		)
+		for (t1_name, t2_name), e in events.items():
+			if util.compute_profit(e.get_t1_min(), e.get_t2_max()) > 1:
+				bet_requests = [
+					# Favorite.
+					bet_request(
+						e.get_t1_min_sportsbook(),
+						e.get_t1_name(),
+						e.get_t1_min(),
+						util.compute_favorite_wager(bet_amt, e.get_t1_min(), e.get_t2_max)
+					),
+					# Underdog.
+					bet_request(
+						e.get_t2_max_sportsbook(),
+						e.get_t2_name(),
+						e.get_t2_max(),
+						util.compute_underdog_wager(bet_amt, e.get_t1_min(), e.get_t2_max)
+					)
+				]
+				log_arb_found(bet_requests)
+			if util.compute_profit(e.get_t1_max(), e.get_t2_min()) > 1:
+				bet_requests = [
+					# Favorite.
+					bet_request(
+						e.get_t2_min_sportsbook(),
+						e.get_t2_name(),
+						e.get_t2_min(),
+						util.compute_favorite_wager(bet_amt, e.get_t2_min(), e.get_t1_max)
+					),
+					# Underdog.
+					bet_request(
+						e.get_t1_max_sportsbook(),
+						e.get_t1_name(),
+						e.get_t1_max(),
+						util.compute_underdog_wager(bet_amt, e.get_t2_min(), e.get_t1_max)
+					)
+				]
+				log_arb_found(bet_requests)
+
+			logger.log(
+				f'Arbitrage not found. Best odds on {t1_name} are {e.get_t1_min():.2f} ({e.get_t1_min_sportsbook()}) '
+				f'and {e.get_t1_max():.2f} ({e.get_t1_max_sportsbook()}).'
+			)
+			logger.log(
+				f'Best odds on {t2_name} are {e.get_t2_min():.2f} ({e.get_t2_min_sportsbook()}) '
+				f'and {e.get_t2_max():.2f} ({e.get_t2_max_sportsbook()}).'
+			)
+
+
+		return None
+
+	def _get_odds(d: driver):
+		return d.get_odds()
+
+	def _execute_bet(d: driver):
+		return d.execute_bet()
+
+	def execute_bets(self, bet_requests: list[bet_request]):
+		# Assign bet requests to drivers.
+		drivers = []
+		for bet_request in bet_requests:
+			if bet_request.get_sportsbook() not in self.drivers:
+				logger.log_error(f'Invalid bet request. {bet_request.get_sportsbook()} not in drivers.')
 				continue
-
-			favorite_stake = bet_amt / ((favorite_odds/underdog_odds) + 1 )
-			underdog_stake = (bet_amt*favorite_odds) / (favorite_odds + underdog_odds)
-			logger.log(f'Arbitrage found! Bet {favorite_stake:.2f} on {favorite} through {favorite_sportsbook} and ')
-			logger.log(f'{underdog_stake:.2f} on {underdog} through {underdog_sportsbook}.')
-			logger.log(f'underdog_stake = ({bet_amt}*{favorite_odds:.2f}) / ({favorite_odds:.2f} + {underdog_odds:.2f})')
-			logger.log(f'favorite_stake = {bet_amt} / (({favorite_odds:.2f}/{underdog_odds:.2f}) + 1 )')
+			driver = self.drivers[bet_request.get_sportsbook()]
+			driver.set_active_bet_request(bet_request)
+			drivers.append(driver)
+		self._run_on_drivers(engine._execute_bet, drivers)
 
 	def bet(self):
 		idx = 0
-		def task(d: driver):
-			return d.get_odds()
-
 		logger.log('Entering arbitrage monitoring loop.')
 		while True:
 			logger.log(f'Loop {idx}.')
-			results = self._run_on_all_drivers(task)
-			self._find_arbitrage(list(chain.from_iterable(results.values())))
+			results = self._run_on_all_drivers(engine._get_odds)
+			odds = []
+			for driver_odds in results.values():
+				if driver_odds:
+					odds += driver_odds
+			bet_requests = self._find_arbitrage(odds)
+			if bet_requests:
+				self.execute_bets(bet_requests)
 			idx += 1
 
 	def epilogue(self):
