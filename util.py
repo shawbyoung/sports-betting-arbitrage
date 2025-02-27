4a7498d486b2fd66a646c3a8ef65958583d4245b
shawbyoung <shawbyoung@gmail.com>
2025-02-26 20:13:40 -0500

    truncated distribution for _long_t, impl wait_for_element, modularized typing simulation, deleted get method

--- util.py
+++ util.py
@@ -1,10 +1,26 @@
-import numpy as np
 import scipy.stats as stats
 import time
+import logger
+import os
+import fnmatch
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 promotion = None
-user_profile_dir = None
-user_profile = None
+profiles_dir = r"C:\Users\shawb\AppData\Local\Google\Chrome\User Data\\"
+chromedriver_path = 'chromedriver/chromedriver.exe'
+max_login_retries = 2
+
+def find(pattern, path):
+    matches = []
+    # Use scandir for efficient directory listing
+    with os.scandir(path) as entries:
+        for entry in entries:
+            # Compare the entry name against the pattern
+            if fnmatch.fnmatch(entry.name, pattern):
+                # Append the full path
+                matches.append(entry.path)
+    return matches
 
 class american:
     def to_decimal(odds: str) -> float:
@@ -19,7 +35,7 @@ class simulate:
     _t_arr_len = 100
 
     _type_t_mu, _type_t_sig = 0.15, 0.05
-    _type_t_l, _type_t_u = 0, 0.3 
+    _type_t_l, _type_t_u = 0, 0.3
     _type_t = stats.truncnorm((_type_t_l - _type_t_mu) / _type_t_sig, (_type_t_u - _type_t_mu) / _type_t_sig, loc=_type_t_mu, scale=_type_t_sig).rvs(_t_arr_len)
     _type_t_idx = 0
 
@@ -30,7 +46,7 @@ class simulate:
 
     _long_t_mu, _long_t_sig = 3.5, 0.5
     _long_t_l, long_t_u = 2.5, 5
-    _long_t = np.random.normal(3.5, 0.50, _t_arr_len)
+    _long_t = stats.truncnorm((_long_t_l - _long_t_mu) / _long_t_sig, (long_t_u - _long_t_mu) / _long_t_sig, loc=_long_t_mu, scale=_long_t_sig).rvs(_t_arr_len)
     _long_t_idx = 0
 
     # TODO: explore conditional waits via selenium.
@@ -51,11 +67,27 @@ class simulate:
     def long_wait():
         time.sleep(simulate.long_interaction_time())
 
-    def type_in_field(input_element, text) -> float:
-        input_element.clear()
+    def wait_for_element(webdriver, wait_time, by, value):
+        WebDriverWait(webdriver, wait_time).until(
+			EC.presence_of_element_located((by, value))
+		)
+    def _type(input_element, text):
         for c in text:
             time.sleep(simulate.type_char_interaction_time())
             input_element.send_keys(c)
+
+    def clear_and_type_in_field(input_element, text):
+        if input_element.get_attribute("value") == text:
+            return
+
+        input_element.clear()
+        simulate._type(input_element, text)
+
+    def type_in_field(input_element, text) -> None:
+        if input_element.get_attribute("value") == text:
+            return
+
+        simulate._type(input_element, text)
 
     def type_char_interaction_time() -> float:
         t = simulate._type_t[simulate._type_t_idx]
@@ -71,7 +103,3 @@ class simulate:
         t = simulate._long_t[simulate._long_t_idx]
         simulate._long_t_idx = (simulate._long_t_idx + 1) % simulate._t_arr_len
         return t
-
-    def get(center, std):
-        res = np.random.normal(center, std, 1)
-        return res[0]
